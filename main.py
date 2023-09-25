22694ab2bfd3eb9294ea518faa0da076a8b1f821
Thomas Moon <thomasmoon0322@gmail.com>
2023-09-25 15:29:17 -0700

    Made it compatible with new DB

--- main.py
+++ main.py
@@ -46,23 +46,30 @@ login_manager.init_app(app)
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
-
+    # Create Foreign Key, "users.id" the users refers to the tablename of User.
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-
+    # Create reference to the User object. The "posts" refers to the posts property in the User class.
+    author = relationship("User", back_populates="posts")
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(500), nullable=False)
-    comments = relationship("Comment", backref="post")
+    img_url = db.Column(db.String(250), nullable=False)
+    # Parent relationship to the comments
+    comments = relationship("Comment", back_populates="parent_post")
 
 
 class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(500), nullable=False)
-    user_name = db.Column(db.Integer, db.ForeignKey("users.name"))
+    text = db.Column(db.Text, nullable=False)
+    # Child relationship:"users.id" The users refers to the tablename of the User class.
+    # "comments" refers to the comments property in the User class.
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment_author = relationship("User", back_populates="comments")
+    # Child Relationship to the BlogPosts
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
+    parent_post = relationship("BlogPost", back_populates="comments")
 
 
 # TODO: Create a User table for all your registered users.
@@ -77,9 +84,12 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
-    posts = relationship("BlogPost", backref="author")
-    comments = relationship("Comment", backref="user")
+    name = db.Column(db.String(100))
+    # This will act like a list of BlogPost objects attached to each User.
+    # The "author" refers to the author property in the BlogPost class.
+    posts = relationship("BlogPost", back_populates="author")
+    # Parent relationship: "comment_author" refers to the comment_author property in the Comment class.
+    comments = relationship("Comment", back_populates="comment_author")
 
 
 with app.app_context():
